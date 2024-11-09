ce0edcd5836efc002f2fa25fba12ac6090f35c0c
M S <mo.sa963@hotmail.com>
2024-11-09 22:19:36 +0300

    Fix slow mode

--- app/Services/RoomSlowMode.php
+++ app/Services/RoomSlowMode.php
@@ -12,7 +12,6 @@ class RoomSlowMode
     public static function check(Room $room, User $user): bool
     {
         $message = $user->messages()->where("room_id", $room->id)->latest("created_at")->first();
-
-        return !$message || Carbon::now()->diffInSeconds(new Carbon($message->created_at)) >= $room->slow_mode;
+        return !$message || $message->created_at->diffInSeconds(Carbon::now()) >= $room->slow_mode;
     }
 }
